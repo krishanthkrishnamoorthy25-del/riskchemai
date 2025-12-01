b157530ee5b1e8d9f7956e69d6345fdea7a46113
Krishanth Krishnamoorthy <krishanth.krishnamoorthy25@gmail.com>
2025-12-01 22:01:34 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import HeroSection from '@/components/landing/HeroSection';
 import FeaturesGrid from '@/components/landing/FeaturesGrid';
+import AIEngineSection from '@/components/landing/AIEngineSection';
+import DangerDetectionSection from '@/components/landing/DangerDetectionSection';
+import SafetyPackSection from '@/components/landing/SafetyPackSection';
+import UserModesSection from '@/components/landing/UserModesSection';
+import ChemicalGuardianSection from '@/components/landing/ChemicalGuardianSection';
+import PrivacyHighlightSection from '@/components/landing/PrivacyHighlightSection';
 import PricingSection from '@/components/landing/PricingSection';
 import SecuritySection from '@/components/landing/SecuritySection';
 import Footer from '@/components/landing/Footer';
@@ -10,6 +16,12 @@ export default function Home() {
     <div className="min-h-screen">
       <HeroSection />
       <FeaturesGrid />
+      <AIEngineSection />
+      <DangerDetectionSection />
+      <SafetyPackSection />
+      <UserModesSection />
+      <ChemicalGuardianSection />
+      <PrivacyHighlightSection />
       <SecuritySection />
       <PricingSection />
       <Footer />
