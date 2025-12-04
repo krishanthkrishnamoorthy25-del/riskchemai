6a68381d94fb8780ec67570c246a5a05c660f6b4
Krishanth <krishanth.krishnamoorthy25@gmail.com>
2025-12-04 13:07:21 -0600

    Create Login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Login() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-3xl font-bold">Page Login</h1>
+        </div>
+    );
+}
